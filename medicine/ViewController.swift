6178ae521b2b98d4f291b3dd7e6b96149cb3b19f
jamie <lessthandecent@jamies-laptop.local>
2021-11-16 21:04:00 -0800

    Create empty xcode project

---
+++ medicine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  medicine
+//
+//  Created by jamie on 11/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
