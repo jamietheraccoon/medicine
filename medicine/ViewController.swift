940c55265bdfe5e96f94b31f6e4e162070ebd29d
jamietheraccoon <gabe_guitron@yahoo.com>
2021-11-19 13:29:00 -0800

    Implement search bar and add some comments to ViewController.swift

--- medicine/ViewController.swift
+++ medicine/ViewController.swift
@@ -7,16 +7,22 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController, UISearchBarDelegate {
+    @IBOutlet var searchBar: UISearchBar!
+    
+    // list to hold a list of all medicine
     var medicine: [Medicine] = [
-        Medicine(name: "Example medicine 1", file: ""),
-        Medicine(name: "Example medicine 2", file: ""),
-        Medicine(name: "Example medicine 3", file: "")
+        Medicine(name: "example medicine 1", file: ""),
+        Medicine(name: "example medicine 2", file: ""),
+        Medicine(name: "example medicine 3", file: "")
     ]
+    // list to hold search results filtered from original medicine list
     var searchResults: [Medicine] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchBar.delegate = self
         
         searchResults = medicine
     }
@@ -25,15 +31,27 @@ class ViewController: UITableViewController {
         return 1
     }
     
+    // get the number of rows from the number of search results
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searchResults.count
     }
     
+    // displays the rows on the screen
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MedicineCell", for: indexPath)
-        // set cell text row to correspondong medicine name
-        cell.textLabel?.text = searchResults[indexPath.row].name;
+        // set cell text row to corresponding medicine name
+        cell.textLabel?.text = searchResults[indexPath.row].name.capitalized;
         return cell
+    }
+    
+    // implement search bar logic
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // filter medicine so that every element contains searchText
+        searchResults = medicine.filter({ medicine in
+            return medicine.name.contains(searchText.lowercased())
+        })
+        // reload tableview's data
+        tableView.reloadData()
     }
 }
 
