eec98c2a48267bece4b379527fa3ba98e6f0425b
jamietheraccoon <gabe_guitron@yahoo.com>
2021-11-20 13:11:23 -0800

    Create segue from table view cell to new view controller MedicineViewController

---
+++ medicine/MedicineViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonViewController.swift
+//  medicine
+//
+//  Created by jamie on 11/20/21.
+//
+
+import UIKit
+
+class MedicineViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
