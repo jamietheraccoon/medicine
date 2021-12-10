d488948ef40d1e43d7e9a9afd7118201f014fb0e
jamietheraccoon <gabe_guitron@yahoo.com>
2021-12-10 11:28:19 -0800

    create basic implementation of medicine pages, add some constraints

--- medicine/Medicine.swift
+++ medicine/Medicine.swift
@@ -18,3 +18,21 @@ struct MedicineListEntry: Decodable {
     let name: String
     let file: String
 }
+
+// ----------------------------
+// individual medicine json files
+
+struct MedicineData: Decodable {
+    let name: String
+    let concentration: String
+    let routes: [String]
+    
+    struct Dosages: Decodable {
+        let adults: [String]
+        let peds: [String]
+    }
+    
+    let indications: [String]
+    let contraindications: [String]
+    let notes: [String]
+}
--- medicine/MedicineViewController.swift
+++ medicine/MedicineViewController.swift
@@ -9,12 +9,53 @@ import UIKit
 
 class MedicineViewController : UIViewController {
     @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var concLabel: UILabel!
     
     var medicine: MedicineListEntry!
+    var medicineData: MedicineData!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nameLabel.text = medicine.name.capitalized
+        let data = readLocalFile(file: medicine.name)
+        guard let data = data else {
+            return
+        }
+        
+        let medicineData = parse(data: data)
+        guard let medicineData = medicineData else {
+            return
+        }
+        
+        nameLabel.text = medicineData.name
+        concLabel.numberOfLines = 0
+        concLabel.text = medicineData.concentration + "\n second line test"
+        concLabel.sizeToFit()
+        
+    }
+    
+    // read local json file into data
+    func readLocalFile(file: String) -> Data? {
+        do {
+            if let filePath = Bundle.main.path(forResource: file, ofType: "json") {
+                let fileURL = URL(fileURLWithPath: filePath)
+                let data = try Data(contentsOf: fileURL)
+                return data
+            }
+        } catch {
+            print("\(error)")
+        }
+        return nil
+    }
+    
+    // parse JSON data into MedicineList type (to be used for the table view)
+    func parse(data: Data) -> MedicineData? {
+        do {
+            let decodedData = try JSONDecoder().decode(MedicineData.self, from: data)
+            return decodedData
+        } catch {
+            print("\(error)")
+        }
+        return nil
     }
 }
